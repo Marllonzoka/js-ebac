64251698e25dd7e0edddb1bb124e132bfb232b2f
Marllon Zoka <marllon@zoka.com>
2024-05-02 10:41:24 -0300

    atualizacao de scripts

--- scripts.js
+++ scripts.js
@@ -1,25 +1,21 @@
-document.getElementById('meuForm').addEventListener('submit',function(event) {
-    event.preventDefault(); //Evitar o envio do formulário
+documento.getElementById('meuForm').addEventListener('submit', function (evento) {
+    evento.preventDefault(); //Evitar o envio do formulário
 
-    //Obtém os valores dos campos
-    var campoA = parselnt(document.getElementByid('campoA').valve);
-    var campoB = parselnt(document.getElementByid('campoB').valve);
-    var messageElement = document.getElementByid('mensagem');
+    Obtém; os; valores; dos; campos;
+    var campoA = parselnt(documento.getElementByid('campoA').válvula);
+    var campoB = parselnt(documento.getElementByid('campoB').válvula);
+    var messageElement = documento.getElementByid('mensagem');
 
-    //Validação: Verificar se o número B é maior que número A
-if (campoB > campoA){
-    messageElement.textContent = "Formulário válido"
-    messageElement.style.color= "green";
-} else {
-// Se for inválido, exibe uma mensagem negativa
+    Verificar; se; o; número; B; é; maior; que; número; A;
+    if (campoB > campoA) {
+        const newLocal = messageElement.textContent = "Formulário válido";
+        messageElement.estilo.cor = "verde";
+    } mais; {
+        Se; for (inválido, exibe; uma; mensagem) negativa;
 
 
-messageElement.textContent = "Número B deve ser maior que número A";
-messageElement.style.color = red;
-}
+        messageElement.textContent = "Número B deve ser maior que número A";
+        messageElement.estilo.cor = vermelho;
+    }
 
 });
-
-// parágrafo null
-{document.ATTRIBUTE_NODE}
-
